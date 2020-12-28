74fae01e8f7091df3cb3f2bc12f85899a636f267
AishCold <72552823+AishCold@users.noreply.github.com>
2020-12-28 17:38:44 +0000

    Add files via upload

---
+++ Hello, Name! Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Input username");
+        //stores user's input into the string variable 'userName'
+        string userName = Console.ReadLine();
+        Console.WriteLine("Hello, " + userName + "!");
+
+    }
+}
+ 
